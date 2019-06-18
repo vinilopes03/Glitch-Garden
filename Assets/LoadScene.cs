fc6723e8f4722cde8e56c96d55864848b1326296
Vinicius C. Lopes <vinilopes03@gmail.com>
2019-06-18 12:56:18 -0300

    Add background and Canvas Changes
    Add background and Canvas Changes

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    int currentIndex;
+    [SerializeField] int timeToWait = 4;
+
+    private void Start()
+    {
+        currentIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+        
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+    public void LoadSplashScene()
+    {
+        SceneManager.LoadScene("Splash Scene");
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+}
